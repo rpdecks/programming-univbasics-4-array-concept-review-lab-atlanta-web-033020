7b189f3dfed24e8df3fa170a96e8c18d3db6a714
Robert Phillips <rpdecks@gmail.com>
2020-03-11 18:52:24 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,7 @@
 def find_element_index(array, value_to_find)
-  array.length.times { |array[] == (value_to_find)|
+  counter = 0
+  while counter < array.length do 
+    array.length.times { |array[] == (value_to_find)|
   puts array.index
   }
 end
